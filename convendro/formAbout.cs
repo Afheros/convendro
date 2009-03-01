c1e737d5203ad174b384d28f76e3eaad50871eff
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-03-01 03:31:23 +0000

    - Minor changes to About box

--- convendro/formAbout.cs
+++ convendro/formAbout.cs
@@ -5,11 +5,18 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace convendro {
     public partial class frmAbout : Form {
         public frmAbout() {
             InitializeComponent();
+            SetVersion();
+        }
+
+        public void SetVersion() {
+            Version n = Assembly.GetExecutingAssembly().GetName().Version;
+            lblVersion.Text = n.ToString();
         }
     }
 }
