4c4b4219581fd130072f15a96de14fae1ca64a21
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-03-21 12:38:22 +0000

    - Added CommandLine helper class

---
+++ trunk/convendro/Classes/CommandLine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace convendro.Classes {
+    /// <summary>
+    /// CommandLine helper class
+    /// </summary>
+    public static class CommandLine {
+        public static string[] Arguments;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <param name="avalue"></param>
+        /// <returns></returns>
+        public static bool GetArgumentValue(string argument, ref string avalue) {
+            bool b = false;
+
+            int i = Array.IndexOf(Arguments, argument);
+            if (i > -1) {
+                string[] s = Arguments[i].Split('=');
+
+                if (s.Length > 1) {
+                    avalue = s[1];
+                } else {
+                    avalue = null;
+                }
+            }
+
+            return b;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <returns></returns>
+        public static int ArgumentIndex(string argument) {
+            return Array.IndexOf(Arguments, argument);
+        }
+    }
+}
