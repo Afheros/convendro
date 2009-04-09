6a1cc59e7c9a974676a57c1f019bf104aedb4c1d
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-04-09 00:43:26 +0000

    - Added generalized process converter

---
+++ convendro/Classes/Threading/IProcessConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using convendro.Classes.Persistence;
+using System.Threading;
+
+namespace convendro.Classes.Threading {
+
+    /// <summary>
+    /// Generalized ProcessConverter
+    /// </summary>
+    public interface IProcessConverter {
+        public string Executable { get; set; }
+        public MediaFileList MediaFileItems { get; set; }
+        public Thread CurrentThread { get; set; }
+        public bool Execute();
+    }
+}
