1aa926cfd80769b50a789169d97589338375beeb
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-12-04 21:48:53 +0000

    + Fix for Mediaset file open.

--- trunk/convendro/Properties/AssemblyInfo.cs
+++ trunk/convendro/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arthur Hoogervorst")]
 [assembly: AssemblyProduct("Convendro")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright ©  2009-2010")]
 [assembly: AssemblyTrademark("Arthur Hoogervorst")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
