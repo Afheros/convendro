7e851be06ceb66c42db2b72c0d46a8d14d1a0e06
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-12-12 22:30:54 +0000

    + Added branch Antares

---
+++ branches/Antares/convendro/Classes/Comparers/MediaFileOrderSorter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using convendro.Classes.Persistence;
+
+namespace convendro.Classes.Comparers {
+    /// <summary>
+    /// MediaFileOrderSorter.
+    /// </summary>
+    public class MediaFileOrderSorter : BaseMediaFileSorter {
+        public override int Compare(MediaFile x, MediaFile y) {
+            int res = 0;
+
+            if (x != null && y != null) {
+                if (Reverse) {
+                    res = y.Order.CompareTo(x.Order);
+                } else {
+                    res = x.Order.CompareTo(y.Order);
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ branches/Antares/convendro/Classes/Threading/IProcessConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using convendro.Classes.Persistence;
+using System.Threading;
+
+namespace convendro.Classes.Threading {
+
+    /// <summary>
+    /// Generalized ProcessConverter
+    /// </summary>
+    public interface IProcessConverter {
+        bool Execute();
+    }
+}
