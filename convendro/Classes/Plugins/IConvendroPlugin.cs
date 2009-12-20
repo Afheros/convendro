fcdc5b9a81d7dcc6d468e992532c0efda0ceb75f
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-12-20 00:26:57 +0000

    + Initial plugin structure

---
+++ convendro/Classes/Plugins/IConvendroPlugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace convendro.Classes.Plugins {
+
+    public interface IConvendroPlugin {
+        string Name { get;}
+        Guid Guid { get; }
+
+        /// <summary>
+        /// Activates a plugin in the host;
+        /// </summary>
+        void Activate();
+
+        /// <summary>
+        /// Deactivates a plugin in the host;
+        /// </summary>
+        void Deactivate();
+
+        /// <summary>
+        /// Shows the plugin main dialog (if provided)
+        /// </summary>
+        bool Execute();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        IConvendroHost Host { get; set; }
+
+        /// <summary>
+        /// Sets up the plugin
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Destroy the plugin;
+        /// </summary>
+        void Uninitialize();
+    }
+}
