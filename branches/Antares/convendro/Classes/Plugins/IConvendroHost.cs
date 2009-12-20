bbf9681911812b79f1441ca90eca467452091ebd
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-12-20 00:26:57 +0000

    + Initial plugin structure

---
+++ branches/Antares/convendro/Classes/Plugins/IConvendroHost.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace convendro.Classes.Plugins {
+    public interface IConvendroHost {
+        bool Register(IConvendroPlugin plugin);
+    }
+}
