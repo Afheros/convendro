4e25305029873c8536a9fa523c8e4c4755953949
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-12-26 03:41:21 +0000

    + Cleanup and changes.

---
+++ branches/Antares/libconvendro/Plugins/BaseConvendroPlugin.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace libconvendro.Plugins {
+    public class BaseConvendroPlugin : IConvendroPlugin{
+
+        #region IConvendroPlugin Members
+
+        private string name = null;
+        private readonly Guid guid = Guid.NewGuid();
+        private IConvendroHost host;
+
+        public string Name {
+            get { return this.name; }
+        }
+
+        public Guid Guid {
+            get { return this.guid; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool Execute() {
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IConvendroHost Host {
+            get {
+                return this.host;
+            }
+            set {
+                this.host = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void setName() {
+            this.name = this.GetType().FullName;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void Initialize() {
+            this.setName();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void Uninitialize() {
+
+        }
+
+        #endregion
+
+        #region IConvendroPlugin Members
+
+
+        public string Description {
+            get { return this.Description; }
+        }
+
+        public string Author {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string CopyrightInformation {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region IConvendroPlugin Members
+
+
+        public Version Version {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+}
---
+++ branches/Antares/libconvendro/Plugins/IConvendroPlugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace libconvendro.Plugins {
+
+    public interface IConvendroPlugin {
+        string Name { get;}
+
+        string Description { get; }
+
+        string Author { get;}
+
+        string CopyrightInformation { get;}
+
+        Version Version { get; }
+
+        Guid Guid { get; }
+
+        /// <summary>
+        /// Shows the plugin main dialog (if provided)
+        /// </summary>
+        bool Execute();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        IConvendroHost Host { get; set; }
+    }
+}
