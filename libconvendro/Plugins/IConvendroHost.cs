69240c0971b5e9932c1fe86d27e121dea3ed1496
arthur.hoogervorst <arthur.hoogervorst@fd0284b8-fbc4-11dd-8259-6defae454439>
2009-12-26 03:41:21 +0000

    + Cleanup and changes.

---
+++ libconvendro/Plugins/IConvendroHost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace libconvendro.Plugins {
+    public interface IConvendroHost {
+        bool Register(IConvendroPlugin plugin);
+        void SetControlsThreading(Boolean var);
+        ListView FileListView { get; set; }
+        StatusStrip Statusbar { get; set; }
+        TextBox LogBox { get; set; }
+    }
+}
